d16a33f7138a8bd94dce5ead816699806222ee98
JohnnieWu <q5896799@gmail.com>
2019-08-28 10:33:10 +0800

    'debug'

--- chenfTensorflow.py
+++ chenfTensorflow.py
@@ -14,6 +14,16 @@ def wait(imgpath):
     print("Wait....")
     sort_trash(imgpath)
 def sort_trash(imgpath):
+    GUI_IN=8#馬達IN1
+    GUI_紙類=26#馬達IN2
+    GUI_塑膠=24#馬達IN3
+    GUI_鐵=13#馬達IN4
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(GUI_IN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(GUI_紙類, GPIO.OUT)
+    GPIO.setup(GUI_塑膠, GPIO.OUT)
+    GPIO.setup(GUI_鐵, GPIO.OUT)
     camera = Camera()
     m=motor()
 	# database = Database()
@@ -23,7 +33,7 @@ def sort_trash(imgpath):
 
     while True:
         
-        GUI_a=GPIO.input(8)
+        GUI_a=GPIO.input(GUI_IN)
         print("self.GUI_a",GUI_a)
         print("等待回應")
 		# wait for camera to detect motion, then sleep for a bit to
@@ -109,16 +119,4 @@ def main():
 	sort_trash('ImageProcessing/img/classificationImage.jpg')
 
 if __name__ == '__main__':
-    i=0
-    GUI_IN=8#馬達IN1
-    GUI_紙類=26#馬達IN2
-    GUI_塑膠=24#馬達IN3
-    GUI_鐵=13#馬達IN4
-    GUI_a=0
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(GUI_IN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(GUI_紙類, GPIO.OUT)
-    GPIO.setup(GUI_塑膠, GPIO.OUT)
-    GPIO.setup(GUI_鐵, GPIO.OUT)
     main()
