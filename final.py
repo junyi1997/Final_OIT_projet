6d4b36a6d7572b4bfc87bd861c0a7ca4a354f22f
JohnnieWu <q5896799@gmail.com>
2019-08-28 09:35:12 +0800

    'debug'

--- final.py
+++ final.py
@@ -1226,6 +1226,7 @@ if __name__ == "__main__":
         'uid': 'seaturtle-105103308'
     }
 })
+    GUI_IN=8#馬達IN1
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(GUI_IN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
