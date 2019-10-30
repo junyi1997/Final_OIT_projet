b1bfe68469bee24f1ebbba9fa86b76bac1416f6a
JohnnieWu <q5896799@gmail.com>
2019-10-30 15:38:03 +0800

    'add_GPIOtest'

--- test_GPIO.py
+++ test_GPIO.py
@@ -9,6 +9,11 @@ import RPi.GPIO as GPIO
 in1_pin=36#一般桶
 in2_pin=38#塑膠桶
 in3_pin=40#紙類桶
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(in1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(in2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(in3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 print(GPIO.input(in1_pin))
 print(GPIO.input(in2_pin))
 print(GPIO.input(in3_pin))
