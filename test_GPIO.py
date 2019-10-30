72671f85573c24f32aac775e39dd4d6ec6853cd2
JohnnieWu <q5896799@gmail.com>
2019-10-30 15:45:02 +0800

    'add_GPIOtest'

--- test_GPIO.py
+++ test_GPIO.py
@@ -6,6 +6,7 @@ Created on Wed Oct 30 15:35:08 2019
 """
 
 import RPi.GPIO as GPIO
+import time
 in1_pin=36#一般桶
 in2_pin=38#塑膠桶
 in3_pin=40#紙類桶
@@ -15,8 +16,6 @@ GPIO.setup(in1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(in2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(in3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
-    print("一般桶定位狀態：{:}".format(GPIO.input(in1_pin)))
-    print("塑膠桶定位狀態：{:}".format(GPIO.input(in2_pin)))
-    print("紙類桶定位狀態：{:}".format(GPIO.input(in3_pin)))
-    print("")
+    print("一般桶定位狀態：{:}塑膠桶定位狀態：{:}紙類桶定位狀態：{:}".format(GPIO.input(in1_pin),GPIO.input(in2_pin),GPIO.input(in3_pin)))
+    time.sleep(1)
 
